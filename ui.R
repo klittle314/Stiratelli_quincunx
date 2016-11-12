41752aac2e4f9a2d2cda82afa002a305d86f50a6
klittle314 <klittle@iecodesign.com>
2016-11-12 15:57:26 -0600

    ui script

---
+++ ui.R
@@ -0,0 +1,57 @@
+# Quincunx shiny app 0.1 
+# Kevin Little, Ph.D.  30 Oct 2016
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+library(shinyjs)
+library(DT)
+library(ggplot2)
+
+shinyUI(fluidPage(
+  useShinyjs(),
+  # Application title
+  titlePanel("Management Data App"),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      actionButton("reset","Tell System to Get Ready!"),
+      br(),
+      br(),
+      div(sliderInput("Meter_Setting",
+                  "Meter Setting:",
+                  min = 15,
+                  max = 45,
+                  value = 30),
+      # Forms that include a submit button do not automatically update their outputs when inputs change, 
+      # rather they wait until the user explicitly clicks the submit button.
+      submitButton("Get value"),
+      br(),
+      helpText("Please click the Tell System to Get Ready button and then click the Get Value button")
+      )
+      
+    ),
+
+    # print the generated value
+    mainPanel(
+      tabsetPanel(type="tabs",
+        tabPanel("Data Display",
+          helpText("Here is the value you requested:"),
+          br(),
+          htmlOutput("Value"),
+          br(),
+          helpText("Here is the count"),
+          br(),
+          htmlOutput("Count")),
+      
+      tabPanel("Admin-T",
+        DT::dataTableOutput("records")),
+      
+      tabPanel("Admin-P",
+          plotOutput("plot1"))
+    )
+  )
+ )
+))
